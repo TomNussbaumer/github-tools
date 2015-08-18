a79b0da2c93d565a044daa7aecb278a9af689f29
Tom Nussbaumer <thomas.nussbaumer@gmx.net>
2015-08-18 22:20:49 +0200

    initial version

---
+++ install-to-bashrc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd $HOME
+if [ -f .bashrc ]; then
+   if [ -z "$(grep 'github-tools-includes' .bashrc)" ]; then
+      echo "[INFO] copying github-tools-include to $HOME"
+      curl -s https://raw.githubusercontent.com/TomNussbaumer/github-tools/master/github-tools-includes > "$HOME/github-tools-includes"
+      echo "[INFO] appending '. ./github-tools-includes' to .bashrc"
+      echo ". ./github-tools-includes" >> .bashrc
+   else
+      echo "[WARNING] github-tools-includes already in .bashrc. Nothing done."
+   fi
+else
+   echo "[ERROR] .bashrc not found. Nothing done."
+fi
