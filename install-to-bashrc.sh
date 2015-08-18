30b4747d5dd6f08df6536e51ea29ef58e50c63cb
Tom Nussbaumer <thomas.nussbaumer@gmx.net>
2015-08-18 22:27:35 +0200

    fix typo

--- install-to-bashrc.sh
+++ install-to-bashrc.sh
@@ -2,7 +2,7 @@
 cd $HOME
 if [ -f .bashrc ]; then
    if [ -z "$(grep 'github-tools-includes' .bashrc)" ]; then
-      echo "[INFO] copying github-tools-include to $HOME"
+      echo "[INFO] copying github-tools-includes to $HOME"
       curl -s https://raw.githubusercontent.com/TomNussbaumer/github-tools/master/github-tools-includes > "$HOME/github-tools-includes"
       echo "[INFO] appending '. ./github-tools-includes' to .bashrc"
       echo ". ./github-tools-includes" >> .bashrc
