7d1288c3c8a1a350895a6ff393bf1813a88cbddf
Tom Nussbaumer <thomas.nussbaumer@gmx.net>
2015-08-18 22:30:22 +0200

    modify fetch logic (overwrite always file at $HOME/github-tools-includes) and source .bashrc after fetching

--- install-to-bashrc.sh
+++ install-to-bashrc.sh
@@ -2,13 +2,12 @@
 cd $HOME
 if [ -f .bashrc ]; then
    if [ -z "$(grep 'github-tools-includes' .bashrc)" ]; then
-      echo "[INFO] copying github-tools-includes to $HOME"
-      curl -s https://raw.githubusercontent.com/TomNussbaumer/github-tools/master/github-tools-includes > "$HOME/github-tools-includes"
       echo "[INFO] appending '. ./github-tools-includes' to .bashrc"
       echo ". ./github-tools-includes" >> .bashrc
-   else
-      echo "[WARNING] github-tools-includes already in .bashrc. Nothing done."
    fi
+   curl -s https://raw.githubusercontent.com/TomNussbaumer/github-tools/master/github-tools-includes > "$HOME/github-tools-includes"
+   echo "[INFO] sourcing .bashrc again."
+   . .bashrc
 else
    echo "[ERROR] .bashrc not found. Nothing done."
 fi
