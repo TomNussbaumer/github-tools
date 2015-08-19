0653efd4b74805ec1a672adfc30428146244b638
Tom Nussbaumer <thomas.nussbaumer@gmx.net>
2015-08-19 18:25:08 +0200

    add local installation feature

--- install-to-bashrc.sh
+++ install-to-bashrc.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
-cd $HOME
+
+GHTFILE="github-tools-includes"
+GHTHOMEFILE="$HOME/$GHTFILE"
+GHTLOCALFILE=
+
+## check if we want to install locally
+[ "$(pwd)" != "$HOME" ] && [ -f $GHTFILE ] && GHTLOCALFILE="$(pwd)/$GHTFILE"
+
+cd "$HOME"
 if [ -f .bashrc ]; then
-   if [ -z "$(grep 'github-tools-includes' .bashrc)" ]; then
-      echo "[INFO] appending '. ./github-tools-includes' to .bashrc"
-      echo ". ./github-tools-includes" >> .bashrc
+   if [ -z "$(grep $GHTFILE .bashrc)" ]; then
+      echo "[INFO] appending \". ./$GHTFILE\" to .bashrc"
+      echo ". ./$GHTFILE" >> .bashrc
    fi
-   curl -s https://raw.githubusercontent.com/TomNussbaumer/github-tools/master/github-tools-includes > "$HOME/github-tools-includes"
-   echo "[INFO] sourcing .bashrc again."
-   . .bashrc
+   if [ -z "$GHTLOCALFILE" ]; then
+     echo "[INFO] fetching from url to [$GHTHOMEFILE]"
+     curl -s https://raw.githubusercontent.com/TomNussbaumer/github-tools/master/github-tools-includes > "$GHTHOMEFILE"
+   else
+     echo "[INFO] local copy from [$GHTLOCALFILE] to [$GHTHOMEFILE]"
+     cp "$GHTLOCALFILE" "$GHTHOMEFILE"
+   fi
+   
+   echo "[INFO] To use the github tools in the current shell please execute the"
+   echo "[INFO] following line to refresh the environment settings:"
+   echo
+   echo "source \"\$HOME/$GHTFILE\""
+   echo
 else
    echo "[ERROR] .bashrc not found. Nothing done."
 fi
